f923726977166976c47f107dc0e8234aa410367b
fabio <parisifabio98@gmail.com>
2021-03-05 15:48:32 +0100

    fuzbiz v1.0 js finito senza bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
 console.log("funzia");
+
+var maxNum = parseInt(prompt("scrivi il numero massimo", "100"));
+var list = [];
+
+for (let i = 1; i < maxNum+1; i++) {
+	
+	if(!(i%3) && !(i%5)){
+		list.push("fizzbuzz");
+	}
+	else if(!(i%3)){
+		list.push("fizz");
+	}
+	else if(!(i%5)){
+		list.push("buzz");
+	}
+	else{
+		list.push(i)
+	}
+}
+document.getElementsByClassName("list")[0].innerHTML = list
+console.log(list);
